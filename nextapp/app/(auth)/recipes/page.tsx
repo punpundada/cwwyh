6463c51fd10e8a2c44a60b194af7f724b5100b96
Parashkar prajwal <prajwalparashkar100@gmail.com>
2024-02-11 02:10:04 +0530

    minor changes

--- nextapp/app/(auth)/recipes/page.tsx
+++ nextapp/app/(auth)/recipes/page.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const RecipePage = () => {
   return (
-    <Container className="flex-col-reverse gap-4 md:flex-row">
-      <Container className="w-3/4">cards</Container>
+    <Container className="flex-col-reverse gap-4 md:flex-row relative">
+      <Container className="w-3/4 sticky">cards</Container>
       <Container className="w-1/4 h-min md:h-full">search filed</Container>
     </Container>
   );
