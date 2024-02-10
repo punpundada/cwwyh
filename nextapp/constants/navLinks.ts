c6b9b3f00b08e8d874417971a5d6a43dc00ecfe7
Parashkar prajwal <prajwalparashkar100@gmail.com>
2024-02-11 02:08:09 +0530

    changed

--- nextapp/constants/navLinks.ts
+++ nextapp/constants/navLinks.ts
@@ -1,7 +1,7 @@
 export const navLinks = [
     { id: 1, name: "Home", path: "/" },
+    { id: 5, name: "Recipes", path: "/recipes" },
     { id: 2, name: "About", path: "/about" },
     { id: 3, name: "Contact", path: "/contact" },
-    { id: 4, name: "Blog", path: "/blog" },
-    // { id: 5, name: "Login", path: "/login" },
+    // { id: 4, name: "Blog", path: "/blog" },
   ] as const;
