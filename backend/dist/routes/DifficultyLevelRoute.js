297b455722d3aebda5c195cc340b88e9da761348
Parashkar prajwal <prajwalparashkar100@gmail.com>
2024-03-14 22:40:04 +0530

    Deleted dist codes

--- backend/dist/routes/DifficultyLevelRoute.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { addLevel, getAllLevels } from "../controller/DifficultyLevelController.js";
-const DifficuiltyLevelRoute = Router();
-DifficuiltyLevelRoute.post("/add", addLevel);
-DifficuiltyLevelRoute.get("/getAll", getAllLevels);
-export default DifficuiltyLevelRoute;
-//# sourceMappingURL=DifficultyLevelRoute.js.map
