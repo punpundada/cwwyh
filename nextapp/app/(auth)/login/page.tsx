048e52ce458114694b5bdd327285910422b17bec
= <=>
2024-06-06 22:50:00 +0530

    changes in login page, signup page from main page.tsx added two svgs for them. increased shadow for cards

--- nextapp/app/(auth)/login/page.tsx
+++ nextapp/app/(auth)/login/page.tsx
@@ -1,12 +1,14 @@
 import Container from "@/components/Container";
 import LoginForm from "@/components/LoginForm";
 import React from "react";
+import loginImg from '@/assets/undraw_fingerprint_login_re_t71l.svg';
+import Image from "next/image";
 
 const LoginPage = () => {
   return (
     <Container className="w-full h-full">
       <Container className="w-1/2 h-full hidden md:flex">
-        image/carousel
+          <Image src={loginImg} width={500} height={500} alt="login image" />
       </Container>
       <Container className="w-full h-full md:w-1/2">
           <LoginForm/>
