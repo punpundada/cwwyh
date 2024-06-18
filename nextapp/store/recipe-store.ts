555c503fb830e12d2a54f7ec386f9fbf53a2d153
Parashkar prajwal <prajwalparashkar100@gmail.com>
2024-06-18 08:29:24 +0530

    changes in recipe by id page , useMediaQuery hook, Main layout page, gloabl.css and added the user type

--- nextapp/store/recipe-store.ts
+++ nextapp/store/recipe-store.ts
@@ -1,21 +1,26 @@
-import {getAllRecipeService , IRecipeRes ,getRecipeById, IReciepById} from "@/services/recipeService";
+import {
+  getAllRecipeService,
+  IRecipeRes,
+  getRecipeById,
+  IReciepById,
+} from "@/services/recipeService";
 import { ApiRes } from "@/types/ApiRes";
 import { IRecipe } from "@/types/IRecipe";
 import { create } from "zustand";
 
 interface recipeStoreProps {
   recipes: IRecipe[];
-  recipe:IRecipe | undefined;
+  recipe: IRecipe | undefined;
   getRecipes: (
     page: string | string[] | 0,
     search: string | string[] | undefined
   ) => Promise<ApiRes<IRecipeRes> | undefined>;
-  getRecipeById : (id:string)=>Promise<ApiRes<IReciepById> | undefined>
+  getRecipeById: (id: string) => Promise<ApiRes<IReciepById> | undefined>;
 }
 
 export const useRecipeStore = create<recipeStoreProps>()((set) => ({
   recipes: [],
-  recipe:undefined,
+  recipe: undefined,
   getRecipes: async (page, search) => {
     try {
       const res = getAllRecipeService(page, search);
@@ -24,17 +29,17 @@ export const useRecipeStore = create<recipeStoreProps>()((set) => ({
       console.log(error);
     }
   },
-  getRecipeById:async (id)=>{
+  getRecipeById: async (id) => {
     try {
       const data = await getRecipeById(id);
-      if(data?.isSuccess){
-        set((s)=>({recipe:data.data.recipes}))
-        return data
+      if (data?.isSuccess) {
+        set((s) => ({ recipe: data.data.recipes }));
+        return data;
       }
-      return undefined
+      return undefined;
     } catch (error) {
-      console.error(error)
-      return undefined
+      console.error(error);
+      return undefined;
     }
-  }
+  },
 }));
