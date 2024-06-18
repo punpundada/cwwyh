536cb3c56d55b7f1a4b590884d91256bb4869aaa
Parashkar prajwal <prajwalparashkar100@gmail.com>
2024-06-18 08:29:24 +0530

    changes in recipe by id page , useMediaQuery hook, Main layout page, gloabl.css and added the user type

--- nextapp/app/recipe/page.tsx
+++ nextapp/app/recipe/page.tsx
@@ -1,3 +1,4 @@
+import BreadCrumbs from "@/components/BreadCrumbs";
 import Container from "@/components/Container";
 import RecipeCard from "@/components/RecipeCard";
 import SearchRecipe from "@/components/SearchRecipe";
@@ -15,6 +16,8 @@ const RecipePage = async ({
   const data = await getAllRecipeService(page, search);
   if (!data) return <>Something went wrong</>;
   return (
+    <Container className="flex-col items-start">
+      {/* <BreadCrumbs names={["Home","Recipe"]} /> */}
     <Container className="flex-col-reverse gap-2 md:flex-row h-full relative">
       <Container className="w-full md:w-3/4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-4 sticky p-8">
         {data.data.recipes.map((x) => (
@@ -24,6 +27,7 @@ const RecipePage = async ({
       <Container className="w-1/4 h-min md:h-full mt-[10rem] md:mt-0 static md:p-8">
         <SearchRecipe/>
       </Container>
+    </Container>
     </Container>
   );
 };
