0b9278ded47c70635a3d01807496ed1835297654
prajwal <prajwal.parashkar@akronsystems.com>
2024-07-18 13:19:10 +0530

    changes in recipe controller and recipe router

--- backend/src/routes/RecipeRoute.ts
+++ backend/src/routes/RecipeRoute.ts
@@ -1,25 +1,16 @@
 import { Router } from "express";
-import {
-  addRecipe,
-  deleteRecipe,
-  getRecipesByIngredients,
-  addRecipeImageUrl,
-  deleteOneImage,
-  getAllRecipes,
-  getOneRecipe,
-  updateRecipe,
-} from "../controller/RecipeController";
+import RecipeController from "../controller/RecipeController";
 import ValidateToken from "../middleware/ValidationTokenHandler";
 
 const RecipieRouter = Router();
 
-RecipieRouter.post("/add", ValidateToken, addRecipe);
-RecipieRouter.post("/update/:id", ValidateToken, updateRecipe);
-RecipieRouter.delete("/delete/:id", ValidateToken, deleteRecipe);
-RecipieRouter.post("/getByIngredients", getRecipesByIngredients);
-RecipieRouter.post("/image/add", ValidateToken, addRecipeImageUrl);
-RecipieRouter.delete("/image/delete", ValidateToken, deleteOneImage);
-RecipieRouter.get("/get", getAllRecipes);
-RecipieRouter.get("/get/:id", getOneRecipe);
+RecipieRouter.post("/add", ValidateToken, RecipeController.addRecipe);
+RecipieRouter.post("/update/:id", ValidateToken, RecipeController.updateRecipe);
+RecipieRouter.delete("/delete/:id", ValidateToken, RecipeController.deleteRecipe);
+RecipieRouter.post("/getByIngredients", RecipeController.getRecipesByIngredients);
+RecipieRouter.post("/image/add", ValidateToken, RecipeController.addRecipeImageUrl);
+RecipieRouter.delete("/image/delete", ValidateToken, RecipeController.deleteOneImage);
+RecipieRouter.get("/get", RecipeController.getAllRecipes);
+RecipieRouter.get("/get/:id", RecipeController.getOneRecipe);
 
 export default RecipieRouter;
